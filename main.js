bbed4b62dbe923de1ce407d9ea5c1f85405ccf1d
saadkhan4 <saadshaheen1327@gmail.com>
2025-08-05 21:10:06 +0500

    created nested list via ClassName

---
+++ main.js
@@ -0,0 +1,31 @@
+const getList = document.getElementsByClassName("list");
+
+const first_ul = document.createElement("ul");
+getList[0].appendChild(first_ul);
+
+const firstList = document.createElement("li");
+firstList.innerText = "BMW";
+first_ul.appendChild(firstList);
+
+const secondList = document.createElement("li");
+secondList.innerText = "VOLVO";
+first_ul.appendChild(secondList);
+
+const thirdList = document.createElement("li");
+thirdList.innerText = "MERCEDES BENZ";
+first_ul.appendChild(thirdList);
+
+const nested_ul = document.createElement("ul");
+secondList.appendChild(nested_ul);
+
+const fourth_list = document.createElement("li");
+fourth_list.innerText = "HONDA";
+nested_ul.appendChild(fourth_list);
+
+const fifth_list = document.createElement("li");
+fifth_list.innerText = "FERRARI";
+nested_ul.appendChild(fifth_list);
+
+const sixth_list = document.createElement("li");
+sixth_list.innerText = "MITSUBISHI MOTORS";
+nested_ul.appendChild(sixth_list);
